c16b851b40cdf763f094c55ddd1ab2bb13b22073
James Kirby <git@jedkirby.com>
2016-05-19 20:00:19 +0100

    Removing the meetup from the home page, it'll be on the about page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,13 +3,7 @@
 
     @include('pages.home.introduction')
     @include('pages.home.projects')
-
-    @if($meetup)
-        @include('pages.home.meetup')
-    @else
-        @include('pages.home.interest')
-    @endif
-
+    @include('pages.home.interest')
     @include('pages.home.tweet')
     @include('pages.home.articles')
 
