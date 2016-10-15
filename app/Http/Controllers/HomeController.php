ddef19318503f5f420680a151aa67ee1ccd00179
James Kirby <git@jedkirby.com>
2016-10-15 10:06:30 +0100

    Updating Leamington to be Stratford Upon Avon in the meta description

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends RootController {
 	public function getHome()
 	{
 
-		$this->setDescription('Website and application developer based in Leamington Spa, UK. An avid blogger of anything related to social media, business, entertainment or technology. Primarily covering Warwickshire, but expanding to the rest of the world to provide a stress free and professional service. Available for hire.');
+		$this->setDescription('Website and application developer based in Stratford Upon Avon, UK. An avid blogger of anything related to social media, business, entertainment or technology. Primarily covering Warwickshire, but expanding to the rest of the world to provide a stress free and professional service. Available for hire.');
 
 		return view('pages.home', [
 			'tweet'    => Twitter::getLatest(),
