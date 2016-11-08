b86ab7f84f399115cb7afab46ac85aa9a99df6bc
James Kirby <james.kirby@jadu.co.uk>
2016-11-08 14:41:12 +0000

    Adding the default abstract test case and the Twitter and Instagram Integration tests

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Test\App;
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\TestCase;
+
+abstract class AbstractTestCase extends TestCase
+{
+
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+}
---
+++ tests/Integrations/InstagramTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Test\App\Integrtions;
+
+use Config;
+use App\Integrations\Instagram;
+use App\Integrations\Instagram\Post;
+use Test\App\AbstractTestCase;
+
+class InstagramTest extends AbstractTestCase
+{
+
+    /**
+     * Default post details.
+     *
+     * @var array
+     */
+    private $postDetails = [
+        'id' => 'BJGINgWDSjR',
+        'link' => 'https://www.instagram.com/p/BJGINgWDSjR/',
+        'images' => [
+            'low_resolution' => [
+                'url' => 'https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/14026723_591240597730224_1149797403_n.jpg?ig_cache_key=MTMxNjc3NjA1MzUwNzQzNDcwNQ%3D%3D.2'
+            ]
+        ],
+        'caption' => [
+            'text' => 'This is the caption text!'
+        ]
+    ];
+
+    /**
+     * Return a post instance with the default details.
+     *
+     * @return Post
+     */
+    private function getPost()
+    {
+        return Instagram::createPostFromArray($this->postDetails);
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnAnInstanceOfTheCorrectClass()
+    {
+        return $this->assertInstanceOf(
+            Post::class,
+            $this->getPost()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itHasTheCorrectId()
+    {
+        return $this->assertEquals(
+            $this->getPost()->getId(),
+            $this->postDetails['id']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itHasTheCorrectLink()
+    {
+        return $this->assertEquals(
+            $this->getPost()->getLink(),
+            $this->postDetails['link']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itHasTheCorrectImage()
+    {
+        return $this->assertEquals(
+            $this->getPost()->getImage(),
+            $this->postDetails['images']['low_resolution']['url']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itHasTheCorrectText()
+    {
+        return $this->assertEquals(
+            $this->getPost()->getText(),
+            $this->postDetails['caption']['text']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itUsesTheDefaultText()
+    {
+
+        $postExpectedDefaultText = 'This is the default text!';
+        $post = Instagram::createPostFromArray([
+            'id' => 'ABC123CDE',
+            'link' => 'http://url.com',
+            'images' => [
+                'low_resolution' => [
+                    'url' => 'http://url.com/image/1'
+                ]
+            ]
+        ]);
+
+        return $this->assertEquals(
+            $post->getText($postExpectedDefaultText),
+            $postExpectedDefaultText
+        );
+
+    }
+
+}
