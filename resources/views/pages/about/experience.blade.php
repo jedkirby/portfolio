13516e55cb2f7d25aea0572bbb0a6330b87a2e54
James Kirby <git@jedkirby.com>
2017-01-05 21:44:28 +0000

    Adding updated information for the about experience section

--- resources/views/pages/about/experience.blade.php
+++ resources/views/pages/about/experience.blade.php
@@ -9,19 +9,21 @@
 			<div class="col--wrapper">
 
 				<div class="col  col--12  experience__wrapper">
-					
+
 					<div class="experience__item  text-right">
 						<div class="experience__icon">
 							<i class="fa fa-square"></i>
 						</div>
 						<div class="experience__desc  experience__desc--left">
 							<h3>
-								<a href="http://jadu.net/spacecraft" target="_blank">Spacecraft Digital</a>
+								<a href="https://www.jadu.net/xforms-professional-forms-solution" target="_blank">Jadu</a>
 							</h3>
 							<div class="experience__meta">
-								<time>July 2015</time> - <time>Present</time>
+								<time>January 2017</time> - <time>Present</time>
 							</div>
-							<p>Deliver beautiful, high-quality software to customers built ontop of the <a href="http://www.jadu.net/solutions" target="_blank">Jadu CMS</a>. Experience by rotating between Agile teams working on a variety of projects and technologies.</p>
+							<p>
+								I've rotated from my role in <a href="https://spacecraft.digital" target="_blank">Spacecraft Digital</a> (See Below) into the <a href="https://www.jadu.net/xforms-professional-forms-solution" target="_blank">XForms Professional</a> team within <a href="https://jadu.net" target="_blank">Jadu</a>. This mean's I'm now a part of Jadu's core, giving me involvement in developing a product. It's also given me the experience working in a fully autonomous agile team consisting of quoting, scrums and retrospectives.
+							</p>
 						</div>
 					</div>
 
@@ -30,28 +32,32 @@
 							<i class="fa fa-square"></i>
 						</div>
 						<div class="experience__desc  experience__desc--right">
+							<h3>
+								<a href="https://spacecraft.digital" target="_blank">Spacecraft Digital</a>
+							</h3>
+							<div class="experience__meta">
+								<time>July 2015</time> - <time>January 2017</time>
+							</div>
+							<p>
+								Primarily working in Professional Services, I built, maintained and supported current and new customers of Spacecraft's. Some examples of sites I've contributed to are <a href="https://www.birmingham.gov.uk" target="_blank">birmingham.gov.uk</a> and <a href="https://www.tjc.edu" target="_blank">tjc.edu</a>. I also spent time developing internal tools which made development and communication across teams easier.
+							</p>
+						</div>
+					</div>
+
+					<div class="experience__item  text-right">
+						<div class="experience__icon">
+							<i class="fa fa-square"></i>
+						</div>
+						<div class="experience__desc  experience__desc--left">
 							<h3>
 								<a href="http://phpwarks.co.uk" target="_blank">PHP Warwickshire</a>
 							</h3>
 							<div class="experience__meta">
 								<time>December 2014</time> - <time>Present</time>
 							</div>
-							<p>As the PHP community grows, new user groups have been popping up all over the world. <a href="https://twitter.com/Ben_Plummer" target="_blank">Ben Plummer</a>, <a href="https://twitter.com/steadweb" target="_blank">Luke Steadman</a> and (me) <a href="{{ Config::get('site.social.streams.twitter.url') }}" target="_blank">James Kirby</a> decided to establish a new PHP user group in the Warwickshire area. We arrange venues, merchandise and arrange a speaker for each last Thursday of the month. Check out our <a href="http://www.meetup.com/PHP-Warwickshire">Meetup</a> account for the next event.</p>
-						</div>
-					</div>
-
-					<div class="experience__item  text-right">
-						<div class="experience__icon">
-							<i class="fa fa-square"></i>
-						</div>
-						<div class="experience__desc  experience__desc--left">
-							<h3>
-								<a href="http://www.propcom.co.uk" target="_blank">Propeller Communications</a>
-							</h3>
-							<div class="experience__meta">
-								<time>September 2013</time> - <time>July 2015</time>
-							</div>
-							<p>Back-end website development primarily using <a href="http://fuelphp.com" target="_blank">FuelPHP</a> as a framework whilst making use of shared packages. Project version control and package management using <a href="https://git-scm.com" target="_blank">Git</a>. Involvement in complete life cycles of projects whilst also providing support for previously developed websites.</p>
+							<p>
+								As the PHP community grows, new user groups have been popping up all over the world. <a href="https://twitter.com/Ben_Plummer" target="_blank">Ben Plummer</a>, <a href="https://twitter.com/steadweb" target="_blank">Luke Steadman</a> and (me) <a href="{{ Config::get('site.social.streams.twitter.url') }}" target="_blank">James Kirby</a> decided to establish a new PHP user group in the Warwickshire area. We arrange venues, merchandise and arrange a speaker for each last Thursday of the month. Check out our <a href="http://www.meetup.com/PHP-Warwickshire">Meetup</a> account for the next event.
+							</p>
 						</div>
 					</div>
 
@@ -61,12 +67,31 @@
 						</div>
 						<div class="experience__desc  experience__desc--right">
 							<h3>
+								<a href="http://www.propcom.co.uk" target="_blank">Propeller Communications</a>
+							</h3>
+							<div class="experience__meta">
+								<time>September 2013</time> - <time>July 2015</time>
+							</div>
+							<p>
+								Back-end website development primarily using <a href="http://fuelphp.com" target="_blank">FuelPHP</a> as a framework whilst making use of shared packages. Project version control and package management using <a href="https://git-scm.com" target="_blank">Git</a>. Involvement in complete life cycles of projects whilst also providing support for previously developed websites. Some examples of the sites I had involvement with are <a href="https://zizzi.co.uk" target="_blank">zizzi.co.uk</a> and <a href="http://galet.com" target="_blank">galet.com</a>.
+							</p>
+						</div>
+					</div>
+
+					<div class="experience__item  text-right">
+						<div class="experience__icon">
+							<i class="fa fa-square"></i>
+						</div>
+						<div class="experience__desc  experience__desc--left">
+							<h3>
 								<a href="http://www.blitzgamesstudios.com" target="_blank">Blitz Games Studios</a>
 							</h3>
 							<div class="experience__meta">
 								<time>April 2011</time> - <time>September 2013</time>
 							</div>
-							<p>Take initial web design concepts and create a website provided to the brief with design and functionality considered. Process redesigns and problems as they are requested, ensuring any modifications I make are fully tested and in working order before released. Ensuring current and new websites are as optimised as possible.</p>
+							<p>
+								Take initial web design concepts and create a website provided to the brief with design and functionality considered. Process redesigns and problems as they are requested, ensuring any modifications I make are fully tested and in working order before released. Ensuring current and new websites are as optimised as possible.
+							</p>
 						</div>
 					</div>
 
