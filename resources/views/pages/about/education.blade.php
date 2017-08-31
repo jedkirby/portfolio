01d59744092ae2f40f43db6c4e2f830337151675
James Kirby <james.kirby@jadu.co.uk>
2017-08-31 14:47:21 +0100

    Adding the scrum master training to my list of education

--- resources/views/pages/about/education.blade.php
+++ resources/views/pages/about/education.blade.php
@@ -1,5 +1,5 @@
 <div class="education">
-    <div class="site__medium">
+    <div class="site__full">
         <div class="col--wrapper">
 
 
@@ -7,16 +7,22 @@
 
                 <div class="education__items  text-center">
 
-                    <div class="education__item  education__item--left">
+                    <div class="education__item">
                         <i class="fa fa-university"></i>
-                        <h3><a href="http://lincoln.ac.uk" target="_blank" title="University of Lincoln">University of Lincoln</a></h3>
+                        <h3><a href="http://lincoln.ac.uk" target="_blank" title="University of Lincoln">Lincoln University</a></h3>
                         <p>BSc, Web Technology</p>
                     </div>
 
-                    <div class="education__item  education__item--right">
-                        <i class="fa fa-building"></i>
+                    <div class="education__item">
+                        <i class="fa fa-graduation-cap"></i>
                         <h3><a href="http://stratford.ac.uk" target="_blank" title="Stratford College">Stratford College</a></h3>
                         <p>BTEC National Diploma, IT Practitioners</p>
+                    </div>
+
+                    <div class="education__item">
+                        <i class="fa fa-book"></i>
+                        <h3><a href="https://www.scrumalliance.org" target="_blank" title="Scrum Alliance">Scrum Alliance</a></h3>
+                        <p>Certified ScrumMaster</p>
                     </div>
 
                 </div>
