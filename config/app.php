8203e5fc35c83a2b102bcbf12ec625a8a14a66a0
James Kirby <james.kirby@tmwi.co.uk>
2019-03-07 11:05:36 +0000

    Adding the database credential detector

--- config/app.php
+++ config/app.php
@@ -131,6 +131,8 @@ return [
     */
 
     'providers' => [
+        App\Domain\Heroku\Provider\DatabaseCredentialServiceProvider::class,
+        // App\Domain\Heroku\Provider\DatabaseDefaultSchemaSettingsServiceProvider::class,
         App\Providers\SlackLoggingServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
