7e29bf14323b56d6072311eb19fd4d38480b5a0f
James Kirby <james.kirby@tmwi.co.uk>
2019-05-07 15:10:00 +0100

    PHPCS

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,35 +1,32 @@
 <?php
 
 /**
- *
  * Biscolab Laravel ReCaptcha configuration file
  * MIT License @ https://github.com/biscolab/laravel-recaptcha/blob/master/LICENSE
  * author: Roberto Belotti - info@robertobelotti.com
- * web : robertobelotti.com, github.com/biscolab
+ * web : robertobelotti.com, github.com/biscolab.
  *
  * To configure correctly please visit https://developers.google.com/recaptcha/docs/start
- *
  */
 
 return [
-
-	/**
-	 *
-	 * The site key
-	 * get site key @ www.google.com/recaptcha/admin
-	 *
-	 */
+    /*
+     *
+     * The site key
+     * get site key @ www.google.com/recaptcha/admin
+     *
+     */
     'api_site_key' => env('RECAPTCHA_SITE_KEY'),
 
-	/**
-	 *
-	 * The secret key
-	 * get secret key @ www.google.com/recaptcha/admin
-	 *
-	 */    
+    /*
+     *
+     * The secret key
+     * get secret key @ www.google.com/recaptcha/admin
+     *
+     */
     'api_secret_key' => env('RECAPTCHA_SECRET_KEY'),
 
-    /**
+    /*
      *
      * ReCATCHA version
      * Supported: "v2", "invisible",
@@ -39,10 +36,10 @@ return [
      */
     'version' => 'invisible',
 
-    /**
+    /*
      *
      * IP addresses for which validation will be skipped
      *
      */
-    'skip_ip' => []    
+    'skip_ip' => [],
 ];
